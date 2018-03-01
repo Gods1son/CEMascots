eb85a779459d551767b81a5edca107f1cda4a699
Gods1son <tolulopeibiyode@gmail.com>
2018-02-28 22:47:44 -0500

    Add files via upload

---
+++ people.js
@@ -0,0 +1,76 @@
+var people = [
+    {
+       "image":"1.jpg",
+        "options":["#REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","PASTOR KAYODE ADESINA"],
+        "category":"CEC"
+    },{
+        "image":"2.jpg",
+        "options":["REV TOM AMEKHIANAN","#PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","PASTOR KAYODE ADESINA"],
+        "category":"CEC"
+    },{
+        "image":"3.jpg",
+        "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","#EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","PASTOR KAYODE ADESINA"],
+        "category":"CEC"
+    },
+    {
+        "image":"4.jpg",
+        "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#PASTOR KAYODE ADESINA"],
+        "category":"CEC"
+    },
+    {
+        "image":"5.jpg",
+        "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","#PASTOR TOM OBIAZI","PASTOR KAYODE ADESINA"],
+        "category":"CEC"
+    },
+    {
+        "image":"6.jpg",
+        "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#PASTOR SARAH OBIAZI"],
+        "category":"CEC"
+    },
+    {
+        "image":"7.jpg",
+        "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#PASTOR LINDA OKOCHA"],
+        "category":"CEC"
+    },
+	{
+       	"image": "1.jpg",
+       	"options": ["REV TOM AMEKHIANAN", "#SINACH", "EVANGELIST EDDY OWASE", "PASTOR TOM OBIAZI", "PASTOR KAYODE ADESINA"],
+       	"category": "Musicians"
+       },
+    {
+        "image":"9.jpg",
+        "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#PASTOR TT (TUOYO TOM) EDUN"],
+        "category":"CEC"
+    },
+    {
+        "image":"10.jpg",
+        "options":["REV TOM AMEKHIANAN","#PASTOR AMBROSE ISESELE","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","PASTOR KAYODE ADESINA"],
+        "category":"CEC"
+       }, 
+	   {
+       	"image": "2.jpg",
+       	"options": ["#JAHDIEL", "PASTOR AMBROSE ISESELE", "EVANGELIST EDDY OWASE", "PASTOR TOM OBIAZI", "PASTOR KAYODE ADESINA"],
+       	"category": "Musicians"
+       },
+	   {
+        "image":"8.jpg",
+        "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#PASTOR DUPE ISESELE"],
+        "category":"CEC"
+    },
+	{
+       	"image": "3.jpg",
+       	"options": ["#EBEN", "PASTOR AMBROSE ISESELE", "EVANGELIST EDDY OWASE", "PASTOR TOM OBIAZI", "PASTOR KAYODE ADESINA"],
+       	"category": "Musicians"
+       },
+];
+
+/*1.       PASTOR CHRIS OYAKHILOME    PRESIDENT BLW
+2.       REV ANITA OYAKHILOME    HEAD OF THE INTL OFFICE
+3.       REV TOM AMEKHIANAN ; PASTOR JOY AMENKHIANAN
+4.       PASTOR RAY OKOCHA; PASTOR LINDA OKOCHA
+5.       EVANGELIST EDDY OWASE; PASTOR MARY OWASE
+6.       PASTOR AMBROSE ISESELE; PASTOR DUPE ISESELE
+7.       PASTOR TOM OBIAZI; PASTOR SARAH OBIAZI
+8.       PASTOR TT (TUOYO TOM) EDUN; PASTOR EWUENA EDUN
+9.       PASTOR KAYODE ADESINA; PASTOR KEMI ADESINA
+10.   PASTOR KEN OYAKHILOME; PASTOR OSE OYAKHILOME*/
