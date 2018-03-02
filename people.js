904b93f7c1bc7fabd477ce1f688f5b9292451f06
Gods1son <tolulopeibiyode@gmail.com>
2018-03-01 20:54:10 -0500

    Add files via upload

---
+++ people.js
@@ -0,0 +1,139 @@
+var people = [
+    {
+       "image":"1.jpg",
+        "options":["#REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","PASTOR KAYODE ADESINA"],
+        "category":"CEC"
+    },{
+        "image":"2.jpg",
+        "options":["PASTOR LINDA OKOCHA","#PASTOR JOY AMEKHIANAN","PASTOR MARY OWASE","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
+        "category":"CEC"
+    },{
+       	"image": "9.jpg",
+       	"options": ["ISRAEL STRONG", "JOE PRAISE", "FRANK EDWARDS", "PEE YUU", "#BUCHI"],
+       	"category": "Musicians"
+       },
+	{
+        "image":"3.jpg",
+        "options":["REV TOM AMEKHIANAN","#PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","PASTOR KAYODE ADESINA"],
+        "category":"CEC"
+    },{
+       	"image": "10.jpg",
+       	"options": ["#CHRIS SHALOM", "JOE PRAISE", "FRANK EDWARDS", "PEE YUU", "BUCHI"],
+       	"category": "Musicians"
+       },
+    {
+        "image":"4.jpg",
+        "options":["#PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","PASTOR MARY OWASE","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
+        "category":"CEC"
+    },{
+       	"image": "4.jpg",
+       	"options": ["#MARTIN PK", "JOE PRAISE", "FRANK EDWARDS", "PEE YUU", "BUCHI"],
+       	"category": "Musicians"
+       },
+    {
+        "image":"5.jpg",
+        "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","#EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","PASTOR KAYODE ADESINA"],
+        "category":"CEC"
+    },{
+       	"image": "7.jpg",
+       	"options": ["#ISRAEL STRONG", "JOE PRAISE", "FRANK EDWARDS", "PEE YUU", "BUCHI"],
+       	"category": "Musicians"
+       },
+    {
+        "image":"6.jpg",
+        "options":["PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","#PASTOR MARY OWASE","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
+        "category":"CEC"
+    },{
+       	"image": "5.jpg",
+       	"options": ["JAHDIEL", "SINACH", "#ADA", "SOPHIA", "EBEN"],
+       	"category": "Musicians"
+       },{
+       	"image": "11.jpg",
+       	"options": ["ISRAEL STRONG", "#T SHARP", "FRANK EDWARDS", "PEE YUU", "BUCHI"],
+       	"category": "Musicians"
+       },
+    {
+        "image":"7.jpg",
+        "options":["REV TOM AMEKHIANAN", "#PASTOR AMBROSE ISESELE", "EVANGELIST EDDY OWASE", "PASTOR TOM OBIAZI", "PASTOR KAYODE ADESINA"],
+        "category":"CEC"
+    },{
+       	"image": "6.jpg",
+       	"options": ["MARTIN PK", "JOE PRAISE", "FRANK EDWARDS", "#PEE YUU", "BUCHI"],
+       	"category": "Musicians"
+       },
+	{
+       	"image": "1.jpg",
+       	"options": ["JAHDIEL", "#SINACH", "ADA", "SOPHIA", "EBEN"],
+       	"category": "Musicians"
+       },
+    {
+        "image":"9.jpg",
+        "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#PASTOR TT EDUN"],
+        "category":"CEC"
+    },{
+       	"image": "8.jpg",
+       	"options": ["ISRAEL STRONG", "JOE PRAISE", "#FRANK EDWARDS", "PEE YUU", "BUCHI"],
+       	"category": "Musicians"
+       },
+	{
+        "image":"15.jpg",
+        "options":["REV TOM AMEKHIANAN","#PASTOR LANRE ALABI","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","PASTOR BIODUN LAWAL"],
+        "category":"CEC"
+       },{
+        "image":"16.jpg",
+        "options":["REV TOM AMEKHIANAN","PASTOR LANRE ALABI","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#PASTOR BIODUN LAWAL"],
+        "category":"CEC"
+       },{
+        "image":"17.jpg",
+        "options":["PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","#PASTOR CHIAMAKA","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
+        "category":"CEC"
+       },
+	{
+        "image":"12.jpg",
+        "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#PASTOR KEN OYAKHILOME"],
+        "category":"CEC"
+       },
+    {
+        "image":"10.jpg",
+        "options":["PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","#PASTOR EWUENA EDUN","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
+        "category":"CEC"
+       },{
+        "image":"13.jpg",
+        "options":["PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","PASTOR DUPE ISESELE","PASTOR SARAH OBIAZI","#PASTOR OSE OYAKHILOME"],
+        "category":"CEC"
+    }, 
+	   {
+       	"image": "2.jpg",
+       	"options": ["#JAHDIEL", "PASTOR AMBROSE ISESELE", "EVANGELIST EDDY OWASE", "PASTOR TOM OBIAZI", "PASTOR KAYODE ADESINA"],
+       	"category": "Musicians"
+       },{
+        "image":"8.jpg",
+        "options":["PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","#PASTOR DUPE ISESELE","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
+        "category":"CEC"
+    },
+	   {
+        "image":"11.jpg",
+        "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#PASTOR KAYODE ADESINA"],
+        "category":"CEC"
+       },{
+        "image":"14.jpg",
+        "options":["PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","#PASTOR DEOLA PHILLIPS","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
+        "category":"CEC"
+    },
+	{
+       	"image": "3.jpg",
+       	"options": ["#EBEN", "PASTOR AMBROSE ISESELE", "EVANGELIST EDDY OWASE", "PASTOR TOM OBIAZI", "PASTOR KAYODE ADESINA"],
+       	"category": "Musicians"
+       }
+];
+
+/*1.       PASTOR CHRIS OYAKHILOME    PRESIDENT BLW
+2.       REV ANITA OYAKHILOME    HEAD OF THE INTL OFFICE
+3.       REV TOM AMEKHIANAN ; PASTOR JOY AMENKHIANAN
+4.       PASTOR RAY OKOCHA; PASTOR LINDA OKOCHA
+5.       EVANGELIST EDDY OWASE; PASTOR MARY OWASE
+6.       PASTOR AMBROSE ISESELE; PASTOR DUPE ISESELE
+7.       PASTOR TOM OBIAZI; PASTOR SARAH OBIAZI
+8.       PASTOR TT (TUOYO TOM) EDUN; PASTOR EWUENA EDUN
+9.       PASTOR KAYODE ADESINA; PASTOR KEMI ADESINA
+10.   PASTOR KEN OYAKHILOME; PASTOR OSE OYAKHILOME*/
