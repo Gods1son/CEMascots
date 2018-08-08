381b28491817d0adcd45e18b9434d339cdf3074c
Gods1son <tolulopeibiyode@gmail.com>
2018-08-07 22:45:07 -0400

    Update people.js

--- people.js
+++ people.js
@@ -2,242 +2,292 @@ var people = [
     {
        "image":"1.jpg",
         "options":["#REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","PASTOR KAYODE ADESINA"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"He is the second in command in the ministry"
     },{
         "image":"2.jpg",
         "options":["PASTOR LINDA OKOCHA","#PASTOR JOY AMEKHIANAN","PASTOR MARY OWASE","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"He is the wife of the second in command in the ministry"
     },{
        	"image": "9.jpg",
        	"options": ["ISRAEL STRONG", "JOE PRAISE", "FRANK EDWARDS", "PEE YUU", "#BUCHI"],
-       	"category": "Musicians"
+       	"category": "Musicians",
+		"info":"He sings reggae and he is a Deacon"
        },
 	{
         "image":"3.jpg",
         "options":["REV TOM AMEKHIANAN","#PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","PASTOR KAYODE ADESINA"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"He was the secretary-general and regional pastor of the UK region and USA international office"
     },{
        	"image": "10.jpg",
        	"options": ["#CHRIS SHALOM", "JOE PRAISE", "FRANK EDWARDS", "PEE YUU", "BUCHI"],
-       	"category": "Musicians"
+       	"category": "Musicians",
+		"info":"He sang the song called You are the reason."
        },
 	   {
 	   	"image": "11.png",
 	   	"options": ["YEAR 2007", "#YEAR 2008", "YEAR 2009", "YEAR 2010", "YEAR 2011"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
 	   {
 	   	"image": "6.png",
 	   	"options": ["YEAR 2002", "#YEAR 2003", "YEAR 2004", "YEAR 2005", "YEAR 2006"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
     {
         "image":"4.jpg",
         "options":["#PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","PASTOR MARY OWASE","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"The wife of one of the CEC"
     },{
        	"image": "4.jpg",
        	"options": ["#MARTIN PK", "JOE PRAISE", "FRANK EDWARDS", "PEE YUU", "BUCHI"],
-       	"category": "Musicians"
+       	"category": "Musicians",
+		"info":"He is from South Africa"
        },
 	   {
 	   	"image": "10.png",
 	   	"options": ["#YEAR 2007", "YEAR 2008", "YEAR 2009", "YEAR 2010", "YEAR 2011"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
 	   {
 	   	"image": "12.png",
 	   	"options": ["YEAR 2007", "YEAR 2008", "#YEAR 2009", "YEAR 2010", "YEAR 2011"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
     {
         "image":"5.jpg",
         "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","#EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","PASTOR KAYODE ADESINA"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"He says I'm alive"
     },{
        	"image": "7.jpg",
        	"options": ["#ISRAEL STRONG", "JOE PRAISE", "FRANK EDWARDS", "PEE YUU", "BUCHI"],
-       	"category": "Musicians"
+       	"category": "Musicians",
+		"info":"He is Pst Chris cousin, award winning artist. He made the song called This year."
        },
     {
         "image":"6.jpg",
         "options":["PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","#PASTOR MARY OWASE","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"From the Benin zone"
     },{
        	"image": "5.jpg",
        	"options": ["JAHDIEL", "SINACH", "#ADA", "SOPHIA", "EBEN"],
-       	"category": "Musicians"
+       	"category": "Musicians",
+		"info":"She has a popular song called Jesus"
        },{
        	"image": "11.jpg",
        	"options": ["ISRAEL STRONG", "#T SHARP", "FRANK EDWARDS", "PEE YUU", "BUCHI"],
-       	"category": "Musicians"
+       	"category": "Musicians",
+		"info":"He made the song called Melody in my heart"
        },
     {
         "image":"7.jpg",
         "options":["REV TOM AMEKHIANAN", "#PASTOR AMBROSE ISESELE", "EVANGELIST EDDY OWASE", "PASTOR TOM OBIAZI", "PASTOR KAYODE ADESINA"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"A CEC member from Warri zone"
     },
 	   {
 	   	"image": "14.png",
 	   	"options": ["YEAR 2007", "YEAR 2008", "YEAR 2009", "YEAR 2010", "#YEAR 2011"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
 	{
        	"image": "6.jpg",
        	"options": ["MARTIN PK", "JOE PRAISE", "FRANK EDWARDS", "#PEE YUU", "BUCHI"],
-       	"category": "Musicians"
+       	"category": "Musicians",
+		"info":"He is also a host on LoveWorld TV"
        },
 	{
        	"image": "1.jpg",
        	"options": ["JAHDIEL", "#SINACH", "ADA", "SOPHIA", "EBEN"],
-       	"category": "Musicians"
+       	"category": "Musicians",
+		"info":"The number one musician in the Christ Embassy Nation"
        },
     {
         "image":"9.jpg",
         "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#PASTOR TT EDUN"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"He calls out most awardees on award night"
     },{
        	"image": "8.jpg",
        	"options": ["ISRAEL STRONG", "JOE PRAISE", "#FRANK EDWARDS", "PEE YUU", "BUCHI"],
-       	"category": "Musicians"
+       	"category": "Musicians",
+		"info":"He made songs with Don Moen"
        },
 	{
 	   "image": "8.png",
 	   "options": ["YEAR 2002", "YEAR 2003", "YEAR 2004", "#YEAR 2005", "YEAR 2006"],
-	   "category": "themeofyear"
+	   "category": "themeofyear",
+	   "info":"The message... was released"
 	   },
 	   {
 	   	"image": "19.png",
 	   	"options": ["YEAR 2015", "#YEAR 2016", "YEAR 2017", "YEAR 2018", "YEAR 2011"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
 	{
         "image":"15.jpg",
         "options":["REV TOM AMEKHIANAN","#PASTOR LANRE ALABI","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","PASTOR BIODUN LAWAL"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"He host a program called Metamorphosis, Pastor of zone 5 Lagos"
        },{
         "image":"16.jpg",
         "options":["REV TOM AMEKHIANAN","PASTOR LANRE ALABI","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#PASTOR BIODUN LAWAL"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"The Pastor of Ghana church"
        },
 	   {
 	   	"image": "4.png",
 	   	"options": ["YEAR 1998", "YEAR 1999", "YEAR 2000", "#YEAR 2001", "YEAR 2002"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
 	{
         "image":"17.jpg",
         "options":["PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","#PASTOR CHIAMAKA","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"She was a former Secretary General"
        },
 	{
 	   "image": "1.png",
 	   "options": ["#YEAR 1998", "YEAR 1999", "YEAR 2000", "YEAR 2001", "YEAR 2002"],
-	   "category": "themeofyear"
+	   "category": "themeofyear",
+	   "info":"The message... was released"
 	  },
 	{
         "image":"12.jpg",
         "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#REV KEN OYAKHILOME"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"The regional Pastor of Canada"
        },
 	{
 	   "image": "9.png",
 	   "options": ["YEAR 2002", "YEAR 2003", "YEAR 2004", "YEAR 2005", "#YEAR 2006"],
-	   "category": "themeofyear"
+	   "category": "themeofyear",
+	   "info":"The message... was released"
 	},
     {
         "image":"10.jpg",
         "options":["PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","#PASTOR EWUENA EDUN","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"Wife of Pastor TT"
        },
 	 {
 	   "image": "17.png",
 	   "options": ["YEAR 2011", "YEAR 2012", "YEAR 2013", "#YEAR 2014", "YEAR 2015"],
-	   "category": "themeofyear"
+	   "category": "themeofyear",
+	   "info":"The message... was released"
 	   },
 	   {
 	   "image": "18.png",
 	   "options": ["YEAR 2011", "YEAR 2012", "YEAR 2013", "YEAR 2014", "#YEAR 2015"],
-	   "category": "themeofyear"
+	   "category": "themeofyear",
+	   "info":"The message... was released"
 	   },
 	{
         "image":"13.jpg",
         "options":["PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","PASTOR DUPE ISESELE","PASTOR SARAH OBIAZI","#PASTOR OSE OYAKHILOME"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"Pastor of United States of America"
     }, 
 	   {
 	   	   	"image": "16.png",
 	   	   	"options": ["YEAR 2011", "YEAR 2012", "#YEAR 2013", "YEAR 2014", "YEAR 2015"],
-	   	   	"category": "themeofyear"
+	   	   	"category": "themeofyear",
+			"info":"The message... was released"
 	   },
 	   {
 	   	"image": "5.png",
 	   	"options": ["YEAR 1998", "YEAR 1999", "YEAR 2000", "YEAR 2001", "#YEAR 2002"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
 	   {
        	"image": "2.jpg",
        	"options": ["#JAHDIEL", "SINACH", "ADA", "SOPHIA", "EBEN"],
-       	"category": "Musicians"
+       	"category": "Musicians",
+		"info":"This my hands"
        },{
         "image":"8.jpg",
         "options":["PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","#PASTOR DUPE ISESELE","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"Wife of CEC member"
     },
 	{
 	   "image": "15.png",
 	   "options": ["YEAR 2011", "#YEAR 2012", "YEAR 2013", "YEAR 2014", "YEAR 2015"],
-	   "category": "themeofyear"
+	   "category": "themeofyear",
+	   "info":"The message... was released"
 	   },
 	   {
         "image":"11.jpeg",
         "options":["REV TOM AMEKHIANAN","PASTOR RAY OKOCHA","EVANGELIST EDDY OWASE","PASTOR TOM OBIAZI","#PASTOR KAYODE ADESINA"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"father of Zoe, our spoken word artist"
        },
 	   {
 	   	"image": "2.png",
 	   	"options": ["YEAR 1998", "#YEAR 1999", "YEAR 2000", "YEAR 2001", "YEAR 2002"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
 	{
         "image":"14.jpg",
         "options":["PASTOR LINDA OKOCHA","PASTOR JOY AMEKHIANAN","#PASTOR DEOLA PHILLIPS","PASTOR SARAH OBIAZI","PASTOR OSE OYAKHILOME"],
-        "category":"Pastors"
+        "category":"Pastors",
+		"info":"CEO of the ministry"
     },
 	{
        	"image": "3.jpg",
        	"options": ["ISRAEL STRONG", "JOE PRAISE", "FRANK EDWARDS", "PEE YUU", "#EBEN"],
-       	"category": "Musicians"
+       	"category": "Musicians",
+		"info":"Made the song called Victory"
        },
 	{
        	"image": "12.jpg",
        	"options": ["ISRAEL STRONG", "#JOE PRAISE", "FRANK EDWARDS", "PEE YUU", "EBEN"],
-       	"category": "Musicians"
+       	"category": "Musicians",
+		"info":"Made a song called Mighty God"
        },
 	   {
 	   	"image": "7.png",
 	   	"options": ["YEAR 2002", "YEAR 2003", "#YEAR 2004", "YEAR 2005", "YEAR 2006"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
 	   {
 	   	"image": "20.png",
 	   	"options": ["YEAR 2015", "YEAR 2016", "#YEAR 2017", "YEAR 2018", "YEAR 2011"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
 	   {
 	   	"image": "3.png",
 	   	"options": ["YEAR 1998", "YEAR 1999", "#YEAR 2000", "YEAR 2001", "YEAR 2002"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
 	   {
 	   	"image": "13.png",
 	   	"options": ["YEAR 2007", "YEAR 2008", "YEAR 2009", "#YEAR 2010", "YEAR 2011"],
-	   	"category": "themeofyear"
+	   	"category": "themeofyear",
+		"info":"The message... was released"
 	   },
 		{
 	 	"image": "21.png",
 	 	"options": ["YEAR 2015", "YEAR 2016", "YEAR 2017", "#YEAR 2018", "YEAR 2011"],
-	 	"category": "themeofyear"
+	 	"category": "themeofyear",
+		"info":"The message... was released"
 		}
 ];
 
